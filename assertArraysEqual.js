6f1cb8d92f1123cdef49a4976586384ea79343e9
SJ <sj.jega2@gmail.com>
2021-08-17 21:10:34 +0000

    assertArraysEqual commit

---
+++ assertArraysEqual.js
@@ -0,0 +1,27 @@
+const eqArrays = function(arr1, arr2) {
+  let match;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      let match = false;
+      return match;
+    } else {
+      match = true;
+    }
+  }
+  return match;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)){
+    console.log(`☑️  Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`⚠️  Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
