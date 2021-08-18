c06d00dc5d93aaf4404a89c60b35f4a1083dd957
SJ <sj.jega2@gmail.com>
2021-08-18 18:10:31 +0000

    countLetter.js commit

---
+++ countLetters.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`☑️  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⚠️  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const countLetters = function (string) {
+  const result = {};  
+  for (const letter of string) {
+    if(letter !== " ") {
+      if (result[letter]) {
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }    
+    }
+  }
+  return result;
+};
+
+
+console.log(countLetters("lighthouse in the house"));
+/*
+{
+  l: 1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1,
+}
+*/
