f4ad3f2dfc69f869259f0ebfe6a380e6a8a30a91
SJ <sj.jega2@gmail.com>
2021-08-23 17:36:30 +0000

    npn initialized the project + installed and implemented mocha/chai test for headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,19 @@
 const head = require('../head');
 const assertEqual = require('../assertEqual');
+const chai = require('chai')
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("head()", () => {
+
+  it("returns 5 for [5,6,7]", () => {
+    chai.assert.strictEqual(head([5,6,7]), 5);
+  });
+
+  it("returns '5' for ['5']", () => {
+    chai.assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it(`returns "Hello" for ["Hello", "Lighthouse", "Labs"]`, () => {
+    chai.assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello'); 
+  });
+
+});
