c158529b6ea487e6d65b4664d360bd9da7c2ec72
SJ <sj.jega2@gmail.com>
2021-08-23 19:36:00 +0000

    cleaned up index.js with ES6 object shorthand

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ const without = require('./without');
 
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  letterPositions: letterPositions,
-  map: map,
-  takeUntil: takeUntil,
-  without: without
+  head,
+  tail,
+  middle,
+  countLetters,
+  countOnly,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil,
+  without
 };
